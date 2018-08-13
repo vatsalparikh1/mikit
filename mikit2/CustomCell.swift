24d1b60709b885b55d9637829d6f9eaf07432aa8
vatsalparikh1 <vatsal@email.unc.edu>
2018-08-13 16:36:50 -0400

    Add files via upload

---
+++ mikit2/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  mikit2
+//
+//  Created by Vatsal Parikh on 4/21/18.
+//  Copyright © 2018 Vatsal Parikh. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+    
+
+}
---
+++ mikit2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mikit2
+//
+//  Created by Vatsal Parikh on 4/20/18.
+//  Copyright © 2018 Vatsal Parikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
